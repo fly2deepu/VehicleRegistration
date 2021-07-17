22eba0ff01569005f001ae9868b9966143bd0965
Deepa Kamalanathan <DeepaK@datacom.co.nz>
2021-07-17 23:27:04 +1200

    Removed unnecessary usings

--- VehicleRegistrationAPI/Models/VehicleDetails.cs
+++ VehicleRegistrationAPI/Models/VehicleDetails.cs
@@ -6,7 +6,6 @@ namespace VehicleRegistrationAPI.Models
 {
     public class VehicleDetails
     {
-
         public string VehicleNo { get; set; }
         public int Year { get; set; }
         public string Make { get; set; }
